1e96e1c319ae18bc037ea6938fab7500294d9071
surya-deepak-pudi <48729818+surya-deepak-pudi@users.noreply.github.com>
2022-02-20 23:50:50 +0530

    Initialize redux

---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+/* eslint-disable global-require */
+const path = require("path")
+const common = require("./webpack.config")
+const { merge } = require("webpack-merge")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+/* eslint-disable global-require */
+
+process.env.NODE_ENV = "development"
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "source-map",
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "public/templates/index.html"),
+      minify: {
+        removeComments: false,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    compress: true,
+    port: 3000,
+    hot: true,
+  },
+})
